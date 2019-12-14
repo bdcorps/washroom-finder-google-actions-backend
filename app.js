1a46a5fff566d0a3d1dac87005fad386fd305491
Sukhpal Saini <bdcorps@gmail.com>
2019-12-14 18:47:39 -0500

    add files

---
+++ app.js
@@ -0,0 +1,27 @@
+var express = require("express"),
+  path = require("path"),
+  bodyParser = require("body-parser"),
+  app = express();
+
+var port = process.env.PORT || 3000;
+
+app.use(
+  bodyParser.urlencoded({
+    extended: false
+  })
+);
+app.use(bodyParser.json());
+
+app.use(express.static(path.resolve(__dirname, "public")));
+
+app.get("/", function(req, res) {
+  res.send("Washroom Google Actions - GET");
+});
+
+app.post("/", function(req, res) {
+  res.send("Washroom Google Actions - POST");
+});
+
+var server = app.listen(port, function() {
+  console.log("The server is running on http://localhost:" + port);
+});
