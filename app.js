3094fa90092a92a7812785ab72d0cd47dcc7967a
Sukhpal Saini <bdcorps@gmail.com>
2019-12-14 19:44:01 -0500

    sample response

--- app.js
+++ app.js
@@ -21,7 +21,71 @@ app.get("/", function(req, res) {
 app.post("/", function(req, res) {
   console.log("req body", JSON.stringify(req.body));
   console.log("req params", JSON.stringify(req.params));
-  res.send("Washroom Google Actions - POST");
+
+  const query = req.body.intent.displayName;
+
+  if (query == "Nearest Washroom") {
+    console.log("washrrom");
+  }
+
+  const result = {
+    fulfillmentText: "This is a text response",
+    fulfillmentMessages: [
+      {
+        card: {
+          title: "card title",
+          subtitle: "card text",
+          imageUri:
+            "https://assistant.google.com/static/images/molecule/Molecule-Formation-stop.png",
+          buttons: [
+            {
+              text: "button text",
+              postback: "https://assistant.google.com/"
+            }
+          ]
+        }
+      }
+    ],
+    source: "example.com",
+    payload: {
+      google: {
+        expectUserResponse: true,
+        richResponse: {
+          items: [
+            {
+              simpleResponse: {
+                textToSpeech: "this is a simple response"
+              }
+            }
+          ]
+        }
+      },
+      facebook: {
+        text: "Hello, Facebook!"
+      },
+      slack: {
+        text: "This is a text response for Slack."
+      }
+    },
+    outputContexts: [
+      {
+        name:
+          "projects/${PROJECT_ID}/agent/sessions/${SESSION_ID}/contexts/context name",
+        lifespanCount: 5,
+        parameters: {
+          param: "param value"
+        }
+      }
+    ],
+    followupEventInput: {
+      name: "event name",
+      languageCode: "en-US",
+      parameters: {
+        param: "param value"
+      }
+    }
+  };
+  res.send(result);
 });
 
 var server = app.listen(port, function() {
